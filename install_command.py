c5ea11ddab50bcf9a4cdac7d4c123cccc0d8ba09
DJ <108424001+Donny-GUI@users.noreply.github.com>
2024-02-14 13:04:44 -0800

    Update install_command.py

--- install_command.py
+++ install_command.py
@@ -4,9 +4,29 @@ import os
 import sys
 
 
+def clean_bash_command(file: str, alias: str):
+    with open(file, "r") as rfile:
+        lines = rfile.readlines()
+    backup = lines.copy()
+    cleaned = []
+    for line in lines:
+        if line.startswith("alias "+ alias):
+            continue
+        if line.startswith("# Python to C command"):
+            continue
+        cleaned.append(line)
+    try: 
+        with open(file, "w") as wfile:
+            wfile.writelines(cleaned)
+    except:
+        with open(file, "w") as wfile:
+            wfile.writelines(backup)
+    os.sync()
+    
 def install_py2pyc_command():
 
     if operating_system != "windows":
+        print(f"[\033[32mUPDATE\033[0m]: installing py2pyc command now.")
         shell_profile_file = os.path.expanduser('~/.bash_profile')
         shell_aliases_file = os.path.expanduser('~/.bash_aliases')
         shell_config_file = os.path.expanduser('~/.bashrc')
@@ -15,10 +35,13 @@ def install_py2pyc_command():
             if os.path.exists(file):
                 target = getcwd() + sep + "mkc.py"
                 if os.path.exists(target):
+                    print(f"[\033[32mUPDATE\033[0m]: writing alias for {target} to {file}")
                     string = '\n# Python to C command \nalias py2pyc="python3 ' + target
+                    clean_bash_command(file, "py2pyc")
                     with open(file, "a") as wfile:
-                        wfile.write(string)
+                        wfile.write(string + "\n\n")
                     os.sync()
+                    print(f"[\033[32mUPDATE\033[0m]: please use the command 'source {file}' to finalize")
                     break
     
     elif operating_system == "windows":
@@ -27,4 +50,3 @@ def install_py2pyc_command():
         with open(pypath, "w") as wfile:
             wfile.write("@echo off\n " + sys.executable + " " + target + " %* \n")
 
-  
